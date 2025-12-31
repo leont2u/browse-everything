ccc57f8e9f5c7b7777a314edd4b6746ed897e81d
Leon Manhimanzi <leontmanhimanz@gmail.com>
2025-12-31 16:48:18 +0200

    Hero section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import NavBar from "./components/NavBar";
+import HeroSection from "./pages/HeroSection";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div className="mx-20 h-min-screen">
+      <NavBar />
+      <HeroSection />
+    </div>
+  );
 }
 
 export default App;
