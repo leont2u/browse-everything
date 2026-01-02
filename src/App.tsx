a6753279862bc2adf71d1ab04ba8aa0d64901a8b
Leon Manhimanzi <leontmanhimanz@gmail.com>
2026-01-02 16:37:38 +0200

    Connect with us section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Benefits from "./pages/Benefits";
+import ConnectWithUs from "./pages/ConnectWithUs";
 import HeroSection from "./pages/HeroSection";
 import TrustedCompanies from "./pages/TrustedCompanies";
 
 function App() {
   return (
-    <div className="mx-20 h-min-screen">
+    <div className="mx-20 h-min-screen ">
       <NavBar />
       <HeroSection />
       <TrustedCompanies />
       <Benefits />
+      <ConnectWithUs />
       <Footer />
     </div>
   );
